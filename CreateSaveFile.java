b7b2f7685dcf0a1f91f6c9118061bde8187e8cec
CodingDiederik <diederikwebster@ziggo.nl>
2023-10-19 11:51:27 +0200

    Created a file reader and writer

---
+++ CreateSaveFile.java
@@ -0,0 +1,32 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.File;
+
+public class CreateSaveFile {
+    private String fileName = "./save.txt"; // Replace with the path to your file
+    private File file = new File(fileName);
+
+    public CreateSaveFile() {
+        try {
+            // Read the file
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            System.out.println("Contents of the file:");
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line); // Print each line to the console
+            }
+            bufferedReader.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new CreateSaveFile();
+    }
+}
