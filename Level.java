03893d7549071adf8aade27b856d198c306197c7
MattiasEXE <matthijs_smulders@kpnmail.nl>
2023-10-28 12:41:24 +0200

    More comments

--- Level.java
+++ Level.java
@@ -8,14 +8,14 @@ public class Level extends JPanel {
     
     int[][] level; // the level is a 2D array of integers, evey coordinate is a block or not a block
 
-    private int BL_WTH = 50; // Width of a block in pixels
-    private int BL_HGT = 50; // Height of a block in pixels
+    private int blWdth = 50; // Width of a block in pixels
+    private int blHgth = 50; // Height of a block in pixels
 
 
     int x0 = 0; //var to scroll the level
 
     private Player player;
-    public int level_number;
+    public int lvlNum;
     public boolean isPaused = false;
     public ReadLevelFile reader = new ReadLevelFile(); // Create a new reader
 
@@ -25,27 +25,32 @@ public class Level extends JPanel {
     
     /**
      * Constructor for objects of class Level.
+     * 
+     * The level is a 2D array of integers.
+     * The dimensions of the level are 64 blocks wide, 12 blocks high.
+     *      The screen is 32 blocks wide, 12 blocks high.
+     * 
+     * @param player The player object, to be able to draw the player
+     * @param lvlNum The level number, to be able to load the correct level
      */
-    public Level(Player player, int level_number) {
+    public Level(Player player, int lvlNum) {
         this.player = player;
-        this.level_number = level_number;
+        this.lvlNum = lvlNum;
         
         // determine dimentions of the level: 64 blocks wide, 12 blocks high
         // The screen is 32 blocks wide, 12 blocks high, so level 1 is 2 screens wide.
         this.level = new int[64][12];
-        
-        // create the level
         createLevel();
     }
 
 
     void createLevel() {
-        if (level_number == 4) {
+        if (lvlNum == 4) {
             gameState = "end";
             return;
         }
 
-        String[] levelConverter = reader.readSaveFile(level_number);
+        String[] levelConverter = reader.readSaveFile(lvlNum);
         if (levelConverter == null) {
             System.out.println("Level not found");
             levelConverter = reader.readSaveFile(1);
@@ -105,7 +110,16 @@ public class Level extends JPanel {
         level[x][y] = 2;
     }
 
-
+    
+    /**
+     * Draw the level depending on the gameState.
+     * 
+     * If the gameState is "running", draw the level and the player.
+     *      Use a for loop to draw the level in a grid of 50x50 pixels.
+     *          Use the x0 variable to scroll the level. 
+     *      Then draw the player outside the for loop,
+     *           so it is drawn without constraints of the level grid
+     */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -113,21 +127,18 @@ public class Level extends JPanel {
         if ("running".equals(gameState)) {
             for (int x = x0 / 50; x < level.length; x++) { 
                 for (int y = 0; y < level[0].length; y++) { 
-
-                    if (level[x][y] == 1) { // check if the coordinate is a block
-                        // draw block the block in the JPanel
-                        g.setColor(Color.BLACK); // set the color of the block 
-                        g.fillRect((x  * BL_WTH) - x0, y * BL_HGT, BL_WTH, 
-                            BL_HGT); // draw the block: LATER: draw the sprite for the block
+                    if (level[x][y] == 1) { 
+                        g.setColor(Color.BLACK);
+                        g.fillRect((x  * blWdth) - x0, y * blHgth, blWdth, 
+                            blHgth); 
                     }
                     if (level[x][y] == 2) {
-
                         g.setColor(Color.GREEN);
-                        g.fillRect((x * BL_WTH) - x0, y * BL_HGT, BL_WTH, BL_HGT);
+                        g.fillRect((x * blWdth) - x0, y * blHgth, blWdth, blHgth);
                     }
                     if (level[x][y] == 3) {
                         g.setColor(Color.RED);
-                        g.fillRect((x  * BL_WTH) - x0, y * BL_HGT, BL_WTH, BL_HGT);
+                        g.fillRect((x  * blWdth) - x0, y * blHgth, blWdth, blHgth);
                     }
 
                 }
@@ -164,6 +175,12 @@ public class Level extends JPanel {
         } 
     }
     
+    /**
+     * Calculate the length of a string for use in paintComponent.
+     * @param s The string to calculate the length of.
+     * @param g The graphics object.
+     * @return The length of the string.
+     */
     int stringLength(String s, Graphics g) {
         return (int) g.getFontMetrics().getStringBounds(s, g).getWidth();
     }
